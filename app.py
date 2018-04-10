334a6dcad3ebd0f6660a32560d92c14478066493
jgertner <gertner.code@gmail.com>
2018-04-10 16:37:43 -0400

    removed https for now

--- app.py
+++ app.py
@@ -368,7 +368,6 @@ def allbooksJSON():
 
 
 if __name__ == '__main__':
-    app.run(ssl_context='adhoc')
     app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
