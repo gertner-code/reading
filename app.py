270ebf0ebe7e42ecf93423b80e76d9b09be9b7fc
jgertner <gertner.code@gmail.com>
2018-04-13 16:41:55 -0400

    formatting

--- app.py
+++ app.py
@@ -3,8 +3,13 @@
 
 """First iteration of website designed to catalog books of interest."""
 
-from flask import Flask, render_template, request, redirect, jsonify, url_for
-from flask import flash
+from flask import (Flask,
+                   render_template,
+                   request,
+                   redirect,
+                   jsonify,
+                   url_for,
+                   flash)
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Genre, Book, User, Base
